c6c33b4fe78a10df3b8480770f2c67a8074a5a0b
Jorge Ibarra <jibarra@ant.com.mx>
2019-08-19 14:13:54 -0500

    nombre de tabla en mayuscula

--- application/core/app/view/getProductData.php
+++ application/core/app/view/getProductData.php
@@ -1,6 +1,6 @@
 <?php
 require '../model/Conection.php';
-$product = R::getAll('SELECT p.*, r.* FROM PRODUCT p inner join rent r on r.product_id = p.id WHERE r.status = 1 and p.id ='.$_GET['id']);
+$product = R::getAll('SELECT p.*, r.* FROM product p inner join rent r on r.product_id = p.id WHERE r.status = 1 and p.id ='.$_GET['id']);
 
 
 
