fcfa654505856468b95cf1d013b1454a94dacd12
jorge <jorge.ibarra.ms@gmail.com>
2019-08-23 13:42:31 -0500

    id faltante

--- application/core/app/assets/refactions.js
+++ application/core/app/assets/refactions.js
@@ -416,7 +416,7 @@ function createReturn(pid){
         contentType:false,
         processData:false,
         success: function (data) {
-          
+
                 location.reload();
 
         }
@@ -460,7 +460,7 @@ for (var i = 0; i < datos.length; i++) {
    $('#marca').val(datos[i].name);
    $('#modelo').val(datos[i].model);
    $('#serie').val(datos[i].serie);
-   $('#serie').val(datos[i].horometro);
+   $('#horo').val(datos[i].horometro);
    $('#tec').val(datos[i].tecnico);
    $('#ob1').val(datos[i].obs_ace1);
    $('#ob2').val(datos[i].obs_ace2);
