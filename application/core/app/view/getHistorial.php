42197ebaeba41d0f3f685b5d54736d72b5785cdc
jorgeibarra14 <37407295+jorgeibarra14@users.noreply.github.com>
2019-08-30 10:27:45 -0500

    Update getHistorial.php

--- application/core/app/view/getHistorial.php
+++ application/core/app/view/getHistorial.php
@@ -1,6 +1,6 @@
 <?php
 require '../model/Conection.php';
-$product = R::getAll('SELECT distinct created, id from returns where product_id =' . $_POST['id']);
+$product = R::getAll('SELECT distinct created, id from returns where product_id =' . $_GET['id']);
 
 $pr = json_encode($product);
 echo $pr;
