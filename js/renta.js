08e9b7d56e1bf84bcf5a59bf48e519ee1b8ab728
DanielVillagran <odvillagrana@gmail.com>
2020-02-03 14:41:48 -0600

    Changes

--- js/renta.js
+++ js/renta.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 				console.log(data);
 				$("#producto").empty().append(data.elemento.name.toUpperCase()+" "+data.elemento.model.toUpperCase()
 					+" "+data.elemento.serie.toUpperCase());
-				$("#desc").empty().append(data.elemento.description.toUpperCase());
+				$("#desc").empty().append(utf8_decode( data.elemento.description.toUpperCase()));
 				$("#rowproductos").append(data.list);
 				$("#capacidad").empty().append(data.elemento.capacity);
 				$("#altura").empty().append(data.elemento.height);
@@ -26,6 +26,50 @@ $(document).ready(function(){
 			}
 		});
 });
+function utf8_decode (strData) { 
+  var tmpArr = []
+  var i = 0
+  var c1 = 0
+  var seqlen = 0
+
+  strData += ''
+
+  while (i < strData.length) {
+    c1 = strData.charCodeAt(i) & 0xFF
+    seqlen = 0
+
+    // https://en.wikipedia.org/wiki/UTF-8#Codepage_layout
+    if (c1 <= 0xBF) {
+      c1 = (c1 & 0x7F)
+      seqlen = 1
+    } else if (c1 <= 0xDF) {
+      c1 = (c1 & 0x1F)
+      seqlen = 2
+    } else if (c1 <= 0xEF) {
+      c1 = (c1 & 0x0F)
+      seqlen = 3
+    } else {
+      c1 = (c1 & 0x07)
+      seqlen = 4
+    }
+
+    for (var ai = 1; ai < seqlen; ++ai) {
+      c1 = ((c1 << 0x06) | (strData.charCodeAt(ai + i) & 0x3F))
+    }
+
+    if (seqlen === 4) {
+      c1 -= 0x10000
+      tmpArr.push(String.fromCharCode(0xD800 | ((c1 >> 10) & 0x3FF)))
+      tmpArr.push(String.fromCharCode(0xDC00 | (c1 & 0x3FF)))
+    } else {
+      tmpArr.push(String.fromCharCode(c1))
+    }
+
+    i += seqlen
+  }
+
+  return tmpArr.join('')
+}
 $("#buscar").keyup(function(event){
 	$.ajax({
 		url: server+"/webserviceapp/get_products.php",
