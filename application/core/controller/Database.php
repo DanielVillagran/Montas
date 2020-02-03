8bfb5e1b340cb81cd46ebca0e1032d72c7368166
DanielVillagran <odvillagrana@gmail.com>
2020-02-03 13:57:14 -0600

    Cahnges

--- application/core/controller/Database.php
+++ application/core/controller/Database.php
@@ -2,22 +2,25 @@
 class Database {
 	public static $db;
 	public static $con;
-	function Database(){
-		$this->user="root";$this->pass="root";$this->host="localhost";$this->ddbb="casamar5_montas";
+	function Database() {
+		$this->user = "casamar5_uno";
+		$this->pass = "daniel200796";
+		$this->host = "localhost";
+		$this->ddbb = "casamar5_montas";
 	}
 
-	function connect(){
-		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
+	function connect() {
+		$con = new mysqli($this->host, $this->user, $this->pass, $this->ddbb);
 		return $con;
 	}
 
-	public static function getCon(){
-		if(self::$con==null && self::$db==null){
+	public static function getCon() {
+		if (self::$con == null && self::$db == null) {
 			self::$db = new Database();
 			self::$con = self::$db->connect();
 		}
 		return self::$con;
 	}
-	
+
 }
 ?>
