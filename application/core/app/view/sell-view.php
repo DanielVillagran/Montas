00dc442caad43f1d6109ee146a396a3c8904493c
DanielVillagran <odvillagrana@gmail.com>
2020-02-18 13:22:42 -0600

    Uploading changes

--- application/core/app/view/sell-view.php
+++ application/core/app/view/sell-view.php
@@ -76,6 +76,7 @@ $iva_val = ConfigurationData::getByPreffix("imp-val")->val;
 <a class="selector" id="qrimg" src="cam.png" onclick="setimg()" align="right">Imagen</a>
 <div id="outdiv">
 </div>
+<video id="preview"></video>
 <div id="result">-- Scaning --</div>
 <canvas id="qr-canvas" width="800" height="600"></canvas>
 
@@ -87,9 +88,15 @@ $iva_val = ConfigurationData::getByPreffix("imp-val")->val;
 <script>
   $(document).ready(function(){
       $("#readqr").click(function(){
+        if(qrreader.style.display=="none"){
+          qrreader.style.display="block";
+          load();
+        }else if(qrreader.style.display=="block"){
+          qrreader.style.display="none";
+        }
         let scanner = new Instascan.Scanner(
             {
-                video: document.getElementById('canvas')
+                video: document.getElementById('preview')
             }
         );
         scanner.addListener('scan', function(content) {
@@ -159,13 +166,13 @@ $(document).ready(function(){
 <?php foreach ($_SESSION["errors"] as $error):
 	$product = ProductData::getById($error["product_id"]);
 	?>
-				<tr class="danger">
-					<td><?php echo $product->id; ?></td>
-					<td><?php echo $product->name; ?></td>
-					<td><b><?php echo $error["message"]; ?></b></td>
-				</tr>
+							<tr class="danger">
+								<td><?php echo $product->id; ?></td>
+								<td><?php echo $product->name; ?></td>
+								<td><b><?php echo $error["message"]; ?></b></td>
+							</tr>
 
-				<?php endforeach;?>
+							<?php endforeach;?>
 </table>
 <?php
 unset($_SESSION["errors"]);
@@ -176,59 +183,59 @@ endif;?>
 <?php if (isset($_SESSION["cart"])):
 	$total = 0;
 	?>
-				<h2>Lista de venta</h2>
-				<div class="box box-primary">
-				<table class="table table-bordered table-hover">
-				<thead>
-					<th style="width:30px;">Codigo</th>
-					<th style="width:30px;">Cantidad</th>
-					<th style="width:30px;">Unidad</th>
-					<th>Producto</th>
-					<th style="width:30px;">Precio Unitario</th>
-					<th style="width:30px;">Precio Total</th>
-					<th ></th>
-				</thead>
-				<?php foreach ($_SESSION["cart"] as $p):
+							<h2>Lista de venta</h2>
+							<div class="box box-primary">
+							<table class="table table-bordered table-hover">
+							<thead>
+								<th style="width:30px;">Codigo</th>
+								<th style="width:30px;">Cantidad</th>
+								<th style="width:30px;">Unidad</th>
+								<th>Producto</th>
+								<th style="width:30px;">Precio Unitario</th>
+								<th style="width:30px;">Precio Total</th>
+								<th ></th>
+							</thead>
+							<?php foreach ($_SESSION["cart"] as $p):
 		$product = ProductData::getById($p["product_id"]);
 		?>
-								<tr >
-									<td><?php echo $product->id; ?></td>
-									<td ><?php echo $p["q"]; ?></td>
-									<td><?php echo $product->unit; ?></td>
-									<td><?php echo $product->name; ?></td>
-									<td><b>$ <?php echo number_format($product->price_out, 2, ".", ","); ?></b></td>
-									<td><b>$ <?php $pt = $product->price_out * $p["q"];
+														<tr >
+															<td><?php echo $product->id; ?></td>
+															<td ><?php echo $p["q"]; ?></td>
+															<td><?php echo $product->unit; ?></td>
+															<td><?php echo $product->name; ?></td>
+															<td><b>$ <?php echo number_format($product->price_out, 2, ".", ","); ?></b></td>
+															<td><b>$ <?php $pt = $product->price_out * $p["q"];
 		$total += $pt;
 		echo number_format($pt, 2, ".", ",");?></b></td>
-									<td style="width:30px;"><a href="index.php?view=clearcart&product_id=<?php echo $product->id; ?>" class="btn btn-danger"><i class="glyphicon glyphicon-remove"></i> Cancelar</a></td>
-								</tr>
+															<td style="width:30px;"><a href="index.php?view=clearcart&product_id=<?php echo $product->id; ?>" class="btn btn-danger"><i class="glyphicon glyphicon-remove"></i> Cancelar</a></td>
+														</tr>
 
-								<?php endforeach;?>
-				</table>
-				</div>
-				<form method="post" class="form-horizontal" id="processsell" action="index.php?view=processsell">
-				<h2>Resumen</h2>
-				<div class="row">
-				<div class="col-md-3">
-				    <label class="control-label">Almacen</label>
-				    <div class="col-lg-12">
-				    <h4 class=""><?php
+														<?php endforeach;?>
+							</table>
+							</div>
+							<form method="post" class="form-horizontal" id="processsell" action="index.php?view=processsell">
+							<h2>Resumen</h2>
+							<div class="row">
+							<div class="col-md-3">
+							    <label class="control-label">Almacen</label>
+							    <div class="col-lg-12">
+							    <h4 class=""><?php
 	echo StockData::getPrincipal()->name;
 	?></h4>
-				    </div>
-				  </div>
+							    </div>
+							  </div>
 
-				<div class="col-md-3">
-				    <label class="control-label">Cliente</label>
-				    <div class="col-lg-12">
-				    <?php
+							<div class="col-md-3">
+							    <label class="control-label">Cliente</label>
+							    <div class="col-lg-12">
+							    <?php
 	$clients = PersonData::getClients();
 	?>
-				    <select name="client_id" id="client_id" class="form-control">
-				    <option value="">-- NINGUNO --</option>
-				    <?php foreach ($clients as $client): ?>
-				    	<option value="<?php echo $client->id; ?>"><?php echo $client->name . " " . $client->lastname; ?></option>
-				    <?php endforeach;?>
+							    <select name="client_id" id="client_id" class="form-control">
+							    <option value="">-- NINGUNO --</option>
+							    <?php foreach ($clients as $client): ?>
+							    	<option value="<?php echo $client->id; ?>"><?php echo $client->name . " " . $client->lastname; ?></option>
+							    <?php endforeach;?>
     	</select>
     </div>
   </div>
